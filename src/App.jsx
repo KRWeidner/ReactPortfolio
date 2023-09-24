dd529c1d167d015d14b2fee68a4cc8436d4f72ad
KatyW <ktabe13@gmail.com>
2023-09-24 15:47:28 -0500

    add header component

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from 'react-router-dom';
 import Nav from './components/NavTabs';
+import Header from "./components/Header";
 
 function App() {
     // The Outlet component will conditionally swap between the different pages according to the URL
     return (
         <>
+            <Header />
             <Nav />
             <main className="mx-3">
                 <Outlet />
