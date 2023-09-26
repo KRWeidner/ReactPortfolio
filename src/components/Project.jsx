91dce9f353a6c874db96c94866dbd32b5854a360
KatyW <ktabe13@gmail.com>
2023-09-25 21:47:26 -0500

    adding portfolio page logic

--- src/components/Project.jsx
+++ src/components/Project.jsx
@@ -1,13 +1,30 @@
+import { useState } from 'react';
 import '../styles/Project.css';
 
 function Project(props) {
+
+    const [isHover, setIsHover] = useState(false);
+
     return (
-      <section className="project">
-        <a href="https://github.com/KRWeidner">
-            <img className='projectImage' src= {`src/assets/${props.imageFile}`} alt="Project Link"></img>
-        </a>
-      </section>
+        <section className="project">
+            <div className='projectImageDiv' onMouseEnter={() => setIsHover(true)} onMouseLeave={() => setIsHover(false)}>
+                <img className='projectImage' src={`src/assets/${props.imageFile}`} alt="Project Link"></img>
+                {isHover && (
+                    <div className='hoverDiv'>
+                        <h3 className='projectName'>{props.name}</h3>
+                        <div className='hoverDivIcon'>
+                            <a href={props.githubLink}>
+                                <img className='projectIcon' src="src/assets/github-mark-white.png" alt="Github Link"></img>
+                            </a>
+                            <a href={props.deployedLink}>
+                                <img className='projectIcon' src="src/assets/webIcon.png" alt="Deployed Link"></img>
+                            </a>
+                        </div>
+                    </div>
+                )}
+            </div>
+        </section>
     );
-  }
-  
-  export default Project;
+}
+
+export default Project;
